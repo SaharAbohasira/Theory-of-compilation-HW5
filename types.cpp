102fa5de517e6519d3b5e530bb88ad83bcf8f95b
Sahar <sahara@campus.technion.ac.il>
2024-08-14 22:36:38 +0300

    test t1

--- types.cpp
+++ types.cpp
@@ -240,7 +240,7 @@ Statement::Statement(Type *type, Node *id, Exp *exp)
         buffer.emit("DEBUG1 "
         if(exp->value == "true")
         {
-            buffer.emit("DEBUG2 "
+            buffer.emit("DEBUG2 ")
             buffer.emit(id_exp->reg + "= add i1 1, 0");
         }
         else if(exp->value == "false")
