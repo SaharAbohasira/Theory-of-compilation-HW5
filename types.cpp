b6286761adf59ec40d9a291d1598258c83cdb1af
Sahar <sahara@campus.technion.ac.il>
2024-08-14 23:07:36 +0300

    test t1

--- types.cpp
+++ types.cpp
@@ -81,12 +81,12 @@ Exp::Exp(const string type, Node *terminal): Node(terminal->value), type(type), 
     }
     if(type == "string")
     {
-        string str = exp->value;
+        string str = terminal->value;
         str.pop_back();
         string temp_reg = codeGenerator.globalFreshVar();
         buffer.emit(temp_reg + " = constant [" + to_string(str.length()) + " x i8] c" + str + "\\00\"");
         reg.replace(0, 1, "%");
-        buffer.emit(temp_reg + ".ptr = getelementptr [" + to_string(str.length())) + " x i8]");
+        buffer.emit(temp_reg + ".ptr = getelementptr [" + to_string(str.length()) + " x i8]");
         reg = temp_reg + ".ptr";
     }
     else if(type == "byte" || type == "int")
