86bcc25e4bcc8ccf04114b87ef555e0cd7eba463
Sahar <sahara@campus.technion.ac.il>
2024-08-14 23:06:55 +0300

    test t1

--- types.cpp
+++ types.cpp
@@ -84,7 +84,7 @@ Exp::Exp(const string type, Node *terminal): Node(terminal->value), type(type), 
         string str = exp->value;
         str.pop_back();
         string temp_reg = codeGenerator.globalFreshVar();
-        buffer.emit(temp_reg + " = constant [" + to_string(str.length())) + " x i8] c" + str + "\\00\"");
+        buffer.emit(temp_reg + " = constant [" + to_string(str.length()) + " x i8] c" + str + "\\00\"");
         reg.replace(0, 1, "%");
         buffer.emit(temp_reg + ".ptr = getelementptr [" + to_string(str.length())) + " x i8]");
         reg = temp_reg + ".ptr";
