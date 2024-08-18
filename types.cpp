1bb06d97e757de3fa98bf8c4f2b3e1bd0c6cc461
galishai <gali.ishai@gmail.com>
2024-08-18 12:05:26 +0300

    testing node reg

--- types.cpp
+++ types.cpp
@@ -24,7 +24,7 @@ Node::Node(const std::string value): value(value)
 
 Node::Node(const Node &node): value(node.value)
 {
-    reg = node.reg;
+    reg = buffer.emit(reg + " = add i32 " + node.reg + ", 0");
 }
 
 Exp::Exp(Node *node1, Node *node2, const std::string op, const std::string type1): isVar(false)
