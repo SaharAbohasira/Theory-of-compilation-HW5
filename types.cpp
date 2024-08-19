15ddce662179760c2aef71e5299815f909b5f884
galishai <gali.ishai@gmail.com>
2024-08-19 15:13:40 +0300

    testing node reg

--- types.cpp
+++ types.cpp
@@ -407,7 +407,7 @@ Call::Call(Node *funcID, Node *node)
         exit(0);
     }
     Exp *exp = dynamic_cast<Exp *>(node);
-    std::cout << exp->reg << endl; #DELETE
+    std::cout << exp->reg << endl;
     /*
     if ( exp->type != symbol->param)
     {
